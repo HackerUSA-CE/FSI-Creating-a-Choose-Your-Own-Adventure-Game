5dffaa9e623d1410f5a84d892f1a86eb0a852db0
Joshua Miles <joshua.m@hackerusa.com>
2020-11-19 14:54:15 -0600

    initial

---
+++ questions.js
@@ -0,0 +1,82 @@
+let questions = [
+    {
+      "question": `What is the first book of the Old Testament?
+        A. Leviticus
+        B. Numbers
+        C. Genesis
+        D. Exodus`,
+      "correctAnswer": "C"
+    },
+    {
+      "question": `How would one say goodbye in Spanish?
+        A. Hola
+        B. Au Revoir
+        C. Salir
+        D. Adiós`,
+      "correctAnswer": "D"
+    },
+    {
+      "question": `What is the French word for 'hat'?
+        A.  Casque
+        B. Chapeau
+        C. Bonnet
+        D. Écharpe`,
+      "correctAnswer": "B"
+    },
+    {
+      "question": `The New York Times slogan is, 'All the News That's Fit to...';
+        A. Digest
+        B. Look
+        C. Read
+        D. Print`,
+      "correctAnswer": "D"
+    },
+    {
+      "question": `Which restaurant's mascot is a clown?
+        A. Burger King
+        B. Sonic
+        C. McDonald's
+        D. Whataburger`,
+      "correctAnswer": "C"
+    },
+    {
+      "question": `Which of the following is the IATA code for Manchester Airport?
+        A. MAN
+        B. EGLL
+        C. LHR
+        D. EGCC`,
+      "correctAnswer": "A"
+    },
+    {
+      "question": `If you are caught &quot;Goldbricking&quot;, what are you doing wrong?
+        A. Stealing
+        B. Cheating
+        C. Slacking
+        D. Smoking`,
+      "correctAnswer": "C"
+    },
+    {
+      "question": `Earth is located in which galaxy?
+        A. The Black Hole
+        B. The Milky Way Galaxy
+        C. The Mars Galaxy
+        D. The Galaxy Note`,
+      "correctAnswer": "B"
+    },
+    {
+      "question": `What are Panama hats made out of?
+        A. Silk
+        B. Hemp
+        C. Flax
+        D. Straw`,
+      "correctAnswer": "D"
+    },
+    {
+      "question": `What kind of aircraft was developed by Igor Sikorsky in the United States in 1942?
+        A. Jet
+        B. Space Capsule
+        C. Helicopter
+        D. Stealth Blimp`,
+      "correctAnswer": "C"
+    }
+  ]
