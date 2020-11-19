8ccf6e55c9a8de2d04451cfb16c2253f014d4160
Joshua Miles <joshua.m@hackerusa.com>
2020-11-19 17:13:14 -0600

    completed

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-// Your Code Here
-
 let userName = window.prompt('Please enter your name:')
 let userScore = 0;
 
 for(let i = 0; i < questions.length; i++){
-    let userAnswer = window.prompt(questions[i].question)
-    if(userAnswer === questions[i].correctAnswer){
+    let question = questions[i]
+    let userAnswer = window.prompt(question.text)
+    if(userAnswer === question.correctAnswer){
         userScore = userScore + 10
     }
 }
@@ -13,9 +12,12 @@ for(let i = 0; i < questions.length; i++){
 window.alert('Your score is: '+userScore)
 
 let serializedGames = localStorage.getItem('games')
-let games = JSON.parse(serializedGames)
-if(games == undefined){
-    games = []
+
+let games;
+if(serializedGames === undefined){
+    games = [] 
+} else {
+    games = JSON.parse(serializedGames)
 }
 
 let game = {
@@ -30,8 +32,9 @@ localStorage.setItem('games', serializedGames)
 
 let highestScoringGame = games[0]
 for(let i = 0; i < games.length; i++){
-    if(games[i].score > highestScoringGame.score){
-        highestScoringGame = games[i]
+    let game = games[i]
+    if(game.score > highestScoringGame.score){
+        highestScoringGame = game
     }
 }
 window.alert('The high score is: '+highestScoringGame.score)
