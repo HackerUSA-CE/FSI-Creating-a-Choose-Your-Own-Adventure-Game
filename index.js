dbd231daafe8c459cb5989d4fbe0ba8814226dec
Tzvetomir Kotzev <tzvetomirkotzev@Tzvetomirs-MacBook-Air.local>
2022-11-07 20:55:27 -0500

    window answer test

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 //Your Code here
+window.prompt('Do you head left or right?')
+let answer  = window.prompt('Do you head left or right?')
+let firstAnswer  = window.prompt('Do you head left or right?')
+console.log(firstAnswer)
